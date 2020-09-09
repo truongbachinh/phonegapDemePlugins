3b82eb4e1ab8da33a5cb21a3aa100f32e2de6eb5
truongbachinh <58037395+truongbachinh@users.noreply.github.com>
2020-09-09 08:30:33 +0700

    av

---
+++ www/js/index.js
@@ -0,0 +1,58 @@
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    receivedEvent: function(id) {
+    }
+};
+
+function callAlert() {
+    navigator.notification.alert(
+        'You are the winner!',  // message
+        alertDismissed,              // callback
+        'Game Over',            // title
+        'Done'            // buttonName
+    );
+}
+function alertDismissed() {
+    alert('You close button');
+
+}
+
+function callConfirm() {
+    navigator.notification.confirm(
+        'You are the winner!', // message
+        onConfirm,            // callback to invoke with index of button pressed
+        'Game Over',           // title
+        ['Restart','Exit']     // buttonLabels
+    );
+}
+function onConfirm(buttonIndex) {
+    alert('You selected button ' + buttonIndex);
+}
+
+function callPrompt() {
+    navigator.notification.prompt(
+        'Please enter your name',  // message
+        onPrompt,                  // callback to invoke
+        'Registration',            // title
+        ['Ok','Exit'],             // buttonLabels
+        'Jane Doe'                 // defaultText
+    );
+}
+function onPrompt(results) {
+    alert("You selected button number " + results.buttonIndex + " and entered " + results.input1);
+}
+function callBeep() {
+    navigator.notification.beep(3);
+}
+
+function callVibration() {
+    navigator.vibrate(3000);
+}
